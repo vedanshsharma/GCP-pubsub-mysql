99bc20b6465e820d80d03129bd6cc51856aa1b99
Vedansh Sharma <svedansh99@gmail.com>
2025-02-21 00:15:35 +0530

    completed producer.py

--- producer.py
+++ producer.py
@@ -15,30 +15,28 @@ topic_path = publisher.topic_path(PROJECT_ID, TOPIC_NAME)
 
 def callback(future):
     try:
-        print(future)
+        # print(future)
         message_id = future.result()
         print(f"Published message with ID: {message_id}")
     except Exception as e:
         print("Error publishing message: {}".format(e))
 
 def produce(last_run,current):
-    sql = "select * from product where last_updated > '{}'".format(last_run)
+    sql = "select * from product "#where last_updated > '{}'".format(last_run)
     db = dbcon()
     db.connect()
     df = pd.DataFrame(db.execute_query(sql))
-    df['last_updated'] = df['last_updated'].astype(str)
-    print(df.head(2))
+    df['last_updated'] = df['last_updated'].astype(int) # since time stamp is being accepted as long
     for row_index, row in df.iterrows():
         message = json.dumps(row.to_dict()).encode('utf-8')
+        print(message)
         try:
-            print("Here")
+            # print("Here")
             future = publisher.publish(topic_path,data = message)
             future.add_done_callback(callback)
             future.result()
         except Exception as e:
             print(f"Exception encountered: {e}")
-        break
-
     
 
 def main():
